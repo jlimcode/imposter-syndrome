f1d8741fea52d49cfe6833cb37af2d6c72fe6c9e
jlimcode <56215940+jlimcode@users.noreply.github.com>
2020-01-02 22:23:31 -0800

    Added location support, now playable

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+const fs = require("fs");
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var bodyParser = require("body-parser");
@@ -12,21 +13,29 @@ app.use(express.static(__dirname + "/static"));
 
 app.use(bodyParser.json());
 
-app.get("/sort", function(req, res) {});
-
 app.post("/start", function(req, res) {
-  console.log(req.body);
-  let location = req.body.location;
-
-  io.emit("start", location);
-  res.json("Started game with location set: " + location);
-
   var spy = clients[Math.floor(clients.length * Math.random())];
   var notSpy = clients.slice();
   notSpy.splice(notSpy.indexOf(spy), 1);
   spy.join("spyroom");
   console.log(Object.keys(spy.rooms));
-  io.to("spyroom").emit("spymessage", "You are a spy");
+  io.to("spyroom").emit("spymessage", "You are the spy");
+
+  let location = req.body.location;
+  io.emit("start", location);
+  res.json("Started game with location set: " + location);
+
+  let contents = fs.readFileSync(
+    __dirname + "/static/locations/" + location.toLowerCase() + ".txt",
+    "utf8"
+  );
+  let locationList = contents.split("\n")
+  let chosenLocation = locationList[Math.floor(locationList.length * Math.random())]
+  console.log("The secret location is: " + chosenLocation)
+  notSpy.forEach((nonSpy) => {
+    nonSpy.join("civilian-room")
+  })
+  io.to("civilian-room").emit("secret-location", chosenLocation)
 });
 
 io.on("connection", function(socket) {
--- static/script.js
+++ static/script.js
@@ -2,28 +2,31 @@ const socket = io();
 
 let landingPage = document.querySelector(".landing-container");
 let homePage = document.querySelector(".home-container");
-homePage.hidden = true;
+homePage.style.display = "none";
 let gamePage = document.querySelector(".game-container");
-gamePage.hidden = true;
+gamePage.style.display = "none";
 
 async function populateLocations(locationSet) {
-    const locationContainer = document.querySelector(".game-location-container")
-    let res = await fetch("/locations/" + locationSet.toLowerCase() + ".txt")
-    let locationList = (await res.text()).split("\n");
-    
-    for(let location of locationList) {
-        let element = document.createElement("div")
-        element.classList.add("game-location")
-        element.classList.add("location-unselected")
-        element.innerHTML = location
-        element.onclick = () => {
-            if(element.classList.contains("location-unselected")) {
-                element.classList.remove("location-unselected")
-                element.classList.add("location-selected")
-            }
-        }
-        locationContainer.appendChild(element)
-    }
+  const locationContainer = document.querySelector(".game-location-container");
+  let res = await fetch("/locations/" + locationSet.toLowerCase() + ".txt");
+  let locationList = (await res.text()).split("\n");
+
+  for (let location of locationList) {
+    let element = document.createElement("div");
+    element.classList.add("game-location");
+    element.classList.add("location-unselected");
+    element.innerHTML = location;
+    element.onclick = () => {
+      if (element.classList.contains("location-unselected")) {
+        element.classList.remove("location-unselected");
+        element.classList.add("location-selected");
+      } else {
+        element.classList.remove("location-selected");
+        element.classList.add("location-unselected");
+      }
+    };
+    locationContainer.appendChild(element);
+  }
 }
 
 document.querySelector(".landing-form").onsubmit = event => {
@@ -31,8 +34,8 @@ document.querySelector(".landing-form").onsubmit = event => {
   let nameInput = document.querySelector(".landing-input");
   socket.emit("nickname", nameInput.value);
   nameInput.value = "";
-  landingPage.hidden = true;
-  homePage.hidden = false;
+  landingPage.style.display = "none";
+  homePage.style.display = "";
 };
 
 document.querySelector(".home-button").onclick = async () => {
@@ -46,8 +49,8 @@ document.querySelector(".home-button").onclick = async () => {
 };
 
 document.querySelector(".game-button").onclick = () => {
-    window.location.href = "/"
-}
+  window.location.href = "/";
+};
 
 socket.on("joined", function(names) {
   console.log(names);
@@ -55,12 +58,18 @@ socket.on("joined", function(names) {
 });
 
 socket.on("start", function(location) {
-  homePage.hidden = true;
-  gamePage.hidden = false;
+  homePage.style.display = "none";
+  gamePage.style.display = "";
   populateLocations(location);
 });
 
 socket.on("spymessage", function(message) {
-    console.log(message);
-    document.querySelector(".game-spy").innerHTML = "You are the spy"
-})
+  console.log(message);
+  document.querySelector(".game-spy-or-location").innerHTML = "You are the spy";
+});
+
+socket.on("secret-location", function(location) {
+  console.log(location);
+  document.querySelector(".game-spy-or-location").innerHTML =
+    "The secret location is: " + location;
+});
